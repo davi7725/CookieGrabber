2b23bba99d4507a98de34ad80f4bc4c27db2a854
davi7725 <davi7725@edu.eal.dk>
2016-11-17 12:20:39 +0100

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Tuple<string, string>> cookies = ReadCookies("ravengaard");
+            foreach (Tuple<string,string> tup in cookies)
+            {
+               // if(tup.Item1.ToString() == "crumb")
+                Console.WriteLine(tup.ToString());
+            }
+            Console.ReadKey();
+        }
+
+        static public IEnumerable<Tuple<string, string>> ReadCookies(string hostName)
+        {
+            if (hostName == null) throw new ArgumentNullException("hostName");
+
+            var dbPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Google\Chrome\User Data\Default\Cookies";
+            if (!System.IO.File.Exists(dbPath)) throw new System.IO.FileNotFoundException("Cant find cookie store", dbPath); // race condition, but i'll risk it
+
+            var connectionString = "Data Source=" + dbPath + ";pooling=false";
+
+            using (var conn = new System.Data.SQLite.SQLiteConnection(connectionString))
+            using (var cmd = conn.CreateCommand())
+            {
+                var prm = cmd.CreateParameter();
+                prm.ParameterName = "hostName";
+                prm.Value = hostName;
+                cmd.Parameters.Add(prm);
+
+                cmd.CommandText = "SELECT name,encrypted_value FROM cookies WHERE host_key LIKE '%" + hostName + "%'";
+
+                conn.Open();
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var encryptedData = (byte[])reader[1];
+                        var decodedData = System.Security.Cryptography.ProtectedData.Unprotect(encryptedData, null, System.Security.Cryptography.DataProtectionScope.CurrentUser);
+                        var plainText = Encoding.ASCII.GetString(decodedData); // Looks like ASCII
+
+                        yield return Tuple.Create(reader.GetString(0), plainText);
+                    }
+                }
+                conn.Close();
+            }
+        }
+    }
+}
